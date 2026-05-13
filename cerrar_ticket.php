62f731244bbf1c89f478420feba6805129ca1ce4
Pablo Zambrano <pablojz70@gmail.com>
2026-05-13 10:05:06 -0400

    Pre-fill solution textarea with 'Cerrado con exito'

--- cerrar_ticket.php
+++ cerrar_ticket.php
@@ -890,7 +890,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['cerrar_ticket'])) {
                         </label>
                         <textarea id="solucion" name="solucion" 
                                   placeholder="Describe detalladamente la solución aplicada o el motivo por el cual se cierra el ticket..."
-                                  ><?php echo htmlspecialchars($_POST['solucion'] ?? ''); ?></textarea>
+                                  ><?php echo htmlspecialchars($_POST['solucion'] ?? 'Cerrado con exito'); ?></textarea>
                         <div id="contador-solucion" style="font-size: 10px; color: #666; margin-top: 3px;">
                             Caracteres: 0
                         </div>
