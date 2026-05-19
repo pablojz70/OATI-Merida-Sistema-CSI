f0b62ab9bbdb4bd563fe7c1bcf9606ef16bc1f93
Pablo Zambrano <pablojz70@gmail.com>
2026-05-19 15:49:52 -0400

    Cleanup: remove test, debug, duplicated & unused files (~100 files)

--- test_directo.php
+++
@@ -1,8 +0,0 @@
-<?php
-// test_directo.php - Prueba directa de la asignación
-session_start();
-$_SESSION['usuario_id'] = 1;
-$_SESSION['privilegio'] = 'admin';
-
-require_once 'procesar_ticket.php';
-?>
